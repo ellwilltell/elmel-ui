d1b661f01a476a88b863edc5642f48c6ee3557cc
Elham Rostami <rostami.elham@ofood.app>
2024-09-24 21:00:43 +0330

    add headlessui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("@headlessui/tailwindcss")({ prefix: "ui" })],
 };
 export default config;
